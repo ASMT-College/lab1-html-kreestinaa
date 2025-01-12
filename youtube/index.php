9bb262e994e5b4471fdee0ee4defb70fce81614a
kreestinaa <bhandarikristina043@gmail.com>
2025-01-12 09:52:01 +0545

    submit youtube assignment

---
+++ youtube/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <h1>hi</hi>
+        <p> hello</p>
+        <?php
+
+        echo 'hello from seerver:' .date('Y-m-d');
+        ?>
+        <br>
+        <script>
+            document.write('hello from client'+new Date())
+            alert('hi')
+            </script>
+</body>
+</html>
